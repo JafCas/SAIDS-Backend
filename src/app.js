e506bca56cef83dfdf47a612ddf3ef3a869037bf
Jafet Castillo <jafetkaid@gmail.com>
2021-10-18 23:46:07 -0500

    Added initial elements to DF and Twilio

--- src/app.js
+++ src/app.js
@@ -1,7 +1,9 @@
+/*
 //DEFINICION DEL SERVIDOR
 const express = require("express");
 const cors = require("cors");
 const app = express(); //Es el servidor
+const dialogflow = require("./dialogflow");
 
 //settings **Configuracion del servidor
 app.set('port', process.env.PORT || 4000);
@@ -15,3 +17,57 @@ app.use('/api/users', require('./routes/users'))
 app.use('/api/records', require('./routes/records'))
 
 module.exports = app;
+*/
+
+// Supports ES6
+// import { create, Whatsapp } from 'venom-bot';
+const uuid = require("uuid");
+const venom = require("venom-bot");
+const dialogflow = require("./dialogflow");
+
+const sessionIds = new Map();
+
+venom
+  .create()
+  .then((client) => start(client))
+  .catch((erro) => {
+    console.log(erro);
+  });
+
+function start(client) {
+  client.onMessage(async (message) => {
+    setSessionAndUser(message.from);
+    let session = sessionIds.get(message.from);
+    //Se guarda en memoria pero se tiene que mover a base de datos
+    let payload = await dialogflow.sendToDialogFlow(message.body, session);
+    let responses = payload.fulfillmentMessages;
+    for (const response of responses) {
+      await sendMessageToWhastApp(client, message, response);
+    }
+    //console.log(message);
+  });
+}
+function sendMessageToWhastApp(client, message, response) {
+  return new Promise((resolve, reject) => {
+    client
+      .sendText(message.from, response.text.text[0])
+      .then((result) => {
+        console.log("Result: ", result); //return object success
+        resolve(result);
+      })
+      .catch((erro) => {
+        console.error("Error when sending: ", erro);
+        reject(erro);
+      });
+  });
+}
+
+async function setSessionAndUser(senderId) {
+  try {
+    if (!sessionIds.has(senderId)) {
+      sessionIds.set(senderId, uuid.v1());
+    }
+  } catch (error) {
+    throw error;
+  }
+}
