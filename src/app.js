4b165f37886c19d2444a722abc26c9eef5cd4506
Jafet Castillo <jafetkaid@gmail.com>
2021-10-20 00:50:27 -0500

    Arreglada interaccion  WA--TW TW--DF

--- src/app.js
+++ src/app.js
@@ -1,9 +1,17 @@
-/*
 //DEFINICION DEL SERVIDOR
 const express = require("express");
 const cors = require("cors");
-const app = express(); //Es el servidor
+const app = express();
+const twilio = require("./twilio");
 const dialogflow = require("./dialogflow");
+const uuid = require("uuid");
+
+const axios = require("axios");
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+const sessionIds = new Map();
 
 //settings **Configuracion del servidor
 app.set('port', process.env.PORT || 4000);
@@ -16,68 +24,25 @@ app.use(express.json()); //Envia archivos en formato json
 app.use('/api/users', require('./routes/users'))
 app.use('/api/records', require('./routes/records'))
 
-module.exports = app;
-*/
-
-// Supports ES6
-// import { create, Whatsapp } from 'venom-bot';
-
-/*const express = require("express");
-const cors = require("cors");
-const app = express(); //Es el servidor
-
-const uuid = require("uuid");
-const venom = require("venom-bot");
-const dialogflow = require("./dialogflow");
-
-//settings **Configuracion del servidor
-app.set("port", process.env.PORT || 4000);
-
-app.use(cors()); //Permite que dos servidores intercambien datos
-app.use(express.json()); //Envia archivos en formato json
-app.use(express.urlencoded({ extended: true }));
-
-app.post("/webhook", function (req, res) {
-  res.sendStatus(200);
-  console.log("req =>", req.body);
+app.get("/webhook", function (req, res) {
+  res.send("Webhook workin'");
 });
 
-const sessionIds = new Map();
+app.post("/webhook", async function (req, res) {
+  console.log("req ->", req.body);
+  let messageComesFromPhone = req.body.WaId;
+  let receivedMessage = req.body.Body;
 
-venom
-  .create()
-  .then((client) => start(client))
-  .catch((erro) => {
-    console.log(erro);
-  });
-
-function start(client) {
-  client.onMessage(async (message) => {
-    setSessionAndUser(message.from);
-    let session = sessionIds.get(message.from);
-    //Se guarda en memoria pero se tiene que mover a base de datos
-    let payload = await dialogflow.sendToDialogFlow(message.body, session);
-    let responses = payload.fulfillmentMessages;
-    for (const response of responses) {
-      await sendMessageToWhastApp(client, message, response);
-    }
-    //console.log(message);
-  });
-}
-function sendMessageToWhastApp(client, message, response) {
-  return new Promise((resolve, reject) => {
-    client
-      .sendText(message.from, response.text.text[0])
-      .then((result) => {
-        console.log("Result: ", result); //return object success
-        resolve(result);
-      })
-      .catch((erro) => {
-        console.error("Error when sending: ", erro);
-        reject(erro);
-      });
-  });
-}
+  setSessionAndUser(messageComesFromPhone);
+  let session = sessionIds.get(messageComesFromPhone);
+  //TODO: Se guarda en memoria pero se tiene que mover a base de datos
+  let payload = await dialogflow.sendToDialogFlow(receivedMessage, session);
+  let responses = payload.fulfillmentMessages;
+  for (const response of responses) {
+    await twilio.sendTextMessage(req.body.WaId, response.text.text[0]);
+  }
+  res.status(200).json({ ok: true, msg: "Mensaje enviado correctamente" });
+});
 
 async function setSessionAndUser(senderId) {
   try {
@@ -88,43 +53,5 @@ async function setSessionAndUser(senderId) {
     throw error;
   }
 }
-*/
 
-const express = require("express");
-const app = express();
-const axios = require("axios");
-const cors = require("cors");
-const twilio = require("./twilio");
-const dialogflow = require("./dialogflow");
-
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
-
-app.get("/webhook", function (req, res) {
-  res.send("Webhook workin'");
-});
-
-app.post("/webhook", async function (req, res) {
-  console.log("req ->", req.body);
-  //twilio.sendTextMessage(req.body.WaId, req.body.Body);
-  let messageComesFromPhone = req.body.WaId;
-  let receivedMessage = req.body.Body;
-
-  let payload = await dialogflow.sendToDialogFlow(receivedMessage, "123123");
-  let responses = payload.fulfillmentMessages;
-  /*for (const response of responses) {
-    await sendMessageToWhastApp(client, message, response);
-  }*/
-  for (const response of responses) {
-    twilio.sendTextMessage(req.body.WaId, response.text.text[0]);
-  }
-  res.status(200).json({ ok: true, msg: "Mensaje enviado correctamente" });
-});
-
-/*async function sendMessageToWhastApp(phone, response) {
-    let payload = await axios.post("")
-}*/
-
-app.listen(6000, () => {
-  console.log("servidor montado en el puerto 6000");
-});
+module.exports = app;
--- src/twilio.js
+++ src/twilio.js
@@ -1,23 +1,4 @@
-/*const accountSid = "AC30eb1ebf5a744f849d3d149ec867f196";
-const authToken = "dd7e8f4aad6c28630858e406b11e16c3";
-//pasar esto a .env
-const client = require("twilio")(accountSid, authToken);
-
-function sendTextMessage(sender, message) {
-  return new Promise((resolve, reject) => {
-    client.messages
-      .create({
-        from: "whatsapp:+14155238886",
-        body: message,
-        to: "whatsapp:+"+sender,
-      })
-      .then((message) => resolve())
-      .catch((err) => reject(err));
-  });
-}
-
-sendTextMessage("525518387942", "Esto esta funcionando!!");*/
-
+//Encargado de la interacción de WhatsApp con Twilio
 require('dotenv').config();
 const accountSid = process.env.TWILIO_ACCOUNT_SID;
 const authToken = process.env.TWILIO_TOKEN;
@@ -30,8 +11,6 @@ function sendTextMessage(sender, message) {
         from: "whatsapp:+14155238886",
         body: message,
         to: "whatsapp:+" + sender,
-        //to: "whatsapp:+" + sender,
-        //to: "whatsapp:+5215518387942",
       })
       .then((message) => resolve())
       .catch((err) => reject(err));
