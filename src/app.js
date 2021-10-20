a42c57c1a09a47f3a9c5b6adfa9d3c455e2e2504
Jafet Castillo <jafetkaid@gmail.com>
2021-10-19 23:54:48 -0500

    Conexion DF-TW-WA

--- src/app.js
+++ src/app.js
@@ -92,17 +92,39 @@ async function setSessionAndUser(senderId) {
 
 const express = require("express");
 const app = express();
+const axios = require("axios");
+const cors = require("cors");
 const twilio = require("./twilio");
+const dialogflow = require("./dialogflow");
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
-app.post("/webhook", function (req, res) {
+app.get("/webhook", function (req, res) {
+  res.send("Webhook workin'");
+});
+
+app.post("/webhook", async function (req, res) {
   console.log("req ->", req.body);
-  twilio.sendTextMessage(req.body.WaId, req.body.Body);
+  //twilio.sendTextMessage(req.body.WaId, req.body.Body);
+  let messageComesFromPhone = req.body.WaId;
+  let receivedMessage = req.body.Body;
+
+  let payload = await dialogflow.sendToDialogFlow(receivedMessage, "123123");
+  let responses = payload.fulfillmentMessages;
+  /*for (const response of responses) {
+    await sendMessageToWhastApp(client, message, response);
+  }*/
+  for (const response of responses) {
+    twilio.sendTextMessage(req.body.WaId, response.text.text[0]);
+  }
   res.status(200).json({ ok: true, msg: "Mensaje enviado correctamente" });
 });
 
-app.listen(3000, () => {
-  console.log("servidor montado en el puerto 3000");
+/*async function sendMessageToWhastApp(phone, response) {
+    let payload = await axios.post("")
+}*/
+
+app.listen(6000, () => {
+  console.log("servidor montado en el puerto 6000");
 });
--- src/twilio.js
+++ src/twilio.js
@@ -18,8 +18,9 @@ function sendTextMessage(sender, message) {
 
 sendTextMessage("525518387942", "Esto esta funcionando!!");*/
 
-const accountSid = "AC30eb1ebf5a744f849d3d149ec867f196";
-const authToken = "dd7e8f4aad6c28630858e406b11e16c3";
+require('dotenv').config();
+const accountSid = process.env.TWILIO_ACCOUNT_SID;
+const authToken = process.env.TWILIO_TOKEN;
 const client = require("twilio")(accountSid, authToken);
 
 function sendTextMessage(sender, message) {
