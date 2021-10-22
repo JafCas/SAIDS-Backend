04954724b08af2b6925fd23d2ddd93d3a15ff0be
Jafet Castillo <jafetkaid@gmail.com>
2021-10-22 00:18:31 -0500

    Agregado trigger inicial de pdf desde whatsapp

--- src/twilio-webhook/dialogflowTwilioWebhook.js
+++ src/twilio-webhook/dialogflowTwilioWebhook.js
@@ -3,6 +3,12 @@ const dialogflowTwilioWebhook = express();
 const twilio = require("./src/twilio");
 const dialogflow = require("./src/dialogflow");
 const uuid = require("uuid");
+const PdfPrinter = require ("pdfmake");
+const fs = require("fs");
+
+const fonts = require("./pdf/components/fonts");
+const styles = require("./pdf/components/styles");
+const {content} = require("./pdf/components/pdfContent");
 
 const sessionIds = new Map();
 
@@ -14,7 +20,7 @@ dialogflowTwilioWebhook.get("/", function (req, res) {
 
 dialogflowTwilioWebhook.post("/", async function (req, res) {
   console.log("req ->", req.body);
-  let messageComesFromPhone = req.body.WaId;
+  messageComesFromPhone = req.body.WaId;
   let receivedMessage = req.body.Body;
 
   setSessionAndUser(messageComesFromPhone);
@@ -23,8 +29,35 @@ dialogflowTwilioWebhook.post("/", async function (req, res) {
   let payload = await dialogflow.sendToDialogFlow(receivedMessage, session);
   let responses = payload.fulfillmentMessages;
   for (const response of responses) {
-    await twilio.sendTextMessage(req.body.WaId, response.text.text[0]);
+    //await twilio.sendTextMessage(req.body.WaId, response.text.text[0]);
   }
+
+  let docDefinition = {
+    content: [
+      {
+        text: "This is a header, using header style.",
+        style: "header",
+      },
+      "Lorem ipsum dolor, sit amet consectetur adipisicing elit. At eveniet animi saepe facilis, aliquid explicabo perspiciatis natus, aspernatur ullam nihil eius numquam, culpa laboriosam asperiores voluptates unde. Architecto, corporis iste?\n\n",
+      { text: "Subheader 1: subheader style", style: "subheader" },
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Facere ad reprehenderit reiciendis quos veritatis perspiciatis odio sint natus. Nisi dicta adipisci dolor rem quas mollitia, quam delectus cum nesciunt! Veritatis.",
+      "Lorem, ipsum dolor sit amet consectetur adipisicing elit. Eum recusandae eius, repudiandae magni cum libero quisquam delectus! Eveniet quo et in, magnam, quia omnis temporibus explicabo porro quas, vel consectetur.\n\n",
+      {
+        text: "This will be a footer, which will be written in small font size",
+        //style: "quote", "small",
+        style: "small"
+      },
+    ],
+    styles: styles
+};
+
+const printer = new PdfPrinter(fonts);
+const fileName = messageComesFromPhone;
+console.log("nombre archivo: ", fileName)
+let pdfDoc = printer.createPdfKitDocument(docDefinition);
+pdfDoc.pipe(fs.createWriteStream("./createdFiles/"+fileName+".pdf"));
+pdfDoc.end();
+
   res.status(200).json({ ok: true, msg: "Mensaje enviado correctamente" });
 });
 
@@ -40,3 +73,4 @@ async function setSessionAndUser(senderId) {
 console.log("Twilio Webhook Workin'")
 
 module.exports = dialogflowTwilioWebhook;
+
---
+++ src/twilio-webhook/pdf/components/fonts.js
@@ -0,0 +1,10 @@
+const path = require("path");
+
+module.exports = {
+  Roboto: {
+    normal: path.resolve("./src/twilio-webhook/pdf/fonts/Roboto-Regular.ttf"),
+    bold: path.resolve("./src/twilio-webhook/pdf/fonts/Roboto-Medium.ttf"),
+    italics: path.resolve("./src/twilio-webhook/pdf/fonts/Roboto-Italic.ttf"),
+    bolditalics: path.resolve("./src/twilio-webhook/pdf/fonts/Roboto-MediumItalic.ttf"),
+  },
+};
